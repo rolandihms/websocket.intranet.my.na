dd5130979061aefd12d944706d1c744cf94452ec
IHMS-ASPIRE <rolandihms@gmail.com>
2018-01-09 21:04:39 +0200

    no message

--- .ebextensions/files/99_restart_services.sh
+++ .ebextensions/files/99_restart_services.sh
@@ -3,9 +3,9 @@
 set -xe
 
 # check if we already have the celeryd service
-/usr/bin/supervisorctl -c /opt/python/etc/supervisord.conf status | grep websocket
+/usr/local/bin/supervisorctl -c /opt/python/etc/supervisord.conf status | grep websocket
 if [[ $? ]]; then
-  /usr/bin/supervisorctl -c /opt/python/etc/supervisord.conf restart websocket
+  /usr/local/bin/supervisorctl -c /opt/python/etc/supervisord.conf restart websocket
 fi
 
 eventHelper.py --msg "Application server successfully restarted." --severity INFO
